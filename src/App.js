c0dbeaf58dde0b0afab9768114e8aa8ca1353c92
SimongSong <ssm19951222@gmail.com>
2020-12-27 15:30:56 -0500

    three.js added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import MainView from './components/MainView';
+// const electron = require("electron")
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is App.js, we will render components here 
-        </p>
-      </header>
+      <MainView></MainView>
     </div>
   );
 }
