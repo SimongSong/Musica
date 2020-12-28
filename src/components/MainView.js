ccc3390c5028144f6393a4a1ff2308508eb42f8b
stevensjcho <stevensjcho@gmail.com>
2020-12-28 12:50:58 -0800

    added a skybox which does not yet load. We have to find a way to link local images

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -26,6 +26,20 @@ function MainView(){
         light.shadow.camera.near = 0.5;
         light.shadow.camera.far = 500;
 
+
+        //Will need to find a way to use local images for the skybox
+        const loader = new THREE.CubeTextureLoader();
+        const texture = loader.load([
+            'corona_bk.jpg',
+            'corona_dn.jpg',
+            'corona_ft.jpg',
+            'corona_lf.jpg',
+            'corona_rt.jpg',
+            'corona_up.jpg',
+        ]);
+        scene.background = texture;
+
+        //Dummy Box to test out shadows, lighting, and positioning
         const geometry = new THREE.BoxGeometry();
         const material = new THREE.MeshLambertMaterial( { color: 0x00ffff } );
         const cube = new THREE.Mesh( geometry, material );
@@ -34,6 +48,7 @@ function MainView(){
         cube.position.set(0, 1.5, 0)
         scene.add( cube );
 
+        //The plane where the game will be played
         const planeGeometry = new THREE.PlaneGeometry( 16, 2000, 8, 8);
         const planeMaterial = new THREE.MeshLambertMaterial( {color: 0xb19cd9, side: THREE.DoubleSide,} );
         const plane = new THREE.Mesh( planeGeometry, planeMaterial );
@@ -44,7 +59,7 @@ function MainView(){
         const lineMaterial = new THREE.LineBasicMaterial( { color: 0x000000} );
         const lineGeometry = new THREE.Geometry();
 
-        
+        //Lines used to drop down the nodes
         lineGeometry.vertices.push( new THREE.Vector3(-5, 0.1, -300));
         lineGeometry.vertices.push( new THREE.Vector3(-5, 1.1, 9));
         lineGeometry.vertices.push( new THREE.Vector3(-2.5, 0.1, -300));
@@ -58,8 +73,9 @@ function MainView(){
 
         var line = new THREE.LineSegments( lineGeometry, lineMaterial );
         scene.add( line );
-        
 
+        
+        //Camera positioning
         camera.position.z = 14;
         camera.position.y = 10;
         camera.lookAt(0, 3, 0);
