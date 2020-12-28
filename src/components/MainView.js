62e43746ac61330124c1200bb9368037ea1dc077
stevensjcho <stevensjcho@gmail.com>
2020-12-27 19:37:27 -0800

    Added a plane, shadows, and lighting

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -8,17 +8,46 @@ function MainView(){
         const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
         const renderer = new THREE.WebGLRenderer();
+        renderer.shadowMap.enabled = true;
+        renderer.shadowMap.type = THREE.PCFSoftShadowMap;
         renderer.setSize( window.innerWidth, window.innerHeight );
         document.body.appendChild( renderer.domElement );
 
+
+        //Create a DirectionalLight and turn on shadows for the light
+        const light = new THREE.DirectionalLight( 0xffffff, 1.2, 100 );
+        light.position.set( -2, 5, 3 );
+        light.castShadow = true;
+        scene.add( light );
+
+        //Set up shadow properties for the light
+        light.shadow.mapSize.width = 512;
+        light.shadow.mapSize.height = 512;
+        light.shadow.camera.near = 0.5;
+        light.shadow.camera.far = 500;
+
         const geometry = new THREE.BoxGeometry();
-        const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        const material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
         const cube = new THREE.Mesh( geometry, material );
+        cube.castShadow = true;
+        cube.receiveShadow = false;
+        cube.position.set(0, 1.5, 0)
         scene.add( cube );
 
-        camera.position.z = 5;
+        const planeGeometry = new THREE.PlaneGeometry( 20, 200, 8, 8);
+        const planeMaterial = new THREE.MeshLambertMaterial( {color: 0xffd1dc, side: THREE.DoubleSide,} );
+        const plane = new THREE.Mesh( planeGeometry, planeMaterial );
+        plane.rotateX(-Math.PI / 2);
+        plane.receiveShadow = true;
+        scene.add( plane );
+        
+        
 
-        const animate = function () {
+        camera.position.z = 12;
+        camera.position.y = 10;
+        camera.lookAt(0, 0, 0)
+
+        const animate = () => {
             requestAnimationFrame( animate );
 
             cube.rotation.x += 0.01;
